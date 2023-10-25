9b76d11b3fd2b7b91516557e9bb7d3665f5133a7
Made By IToncek <toncek2019@post.cz>
2023-10-25 20:42:42 +0200

    Final CI (I hope)

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 cat runtimes.txt | while read p 
 do
-  dotnet publish -c Release -r $p --self-contained false -p:PublishReadyToRun=true
+  dotnet publish -c Release -r $p --self-contained true -p:PublishReadyToRun=true
   find bin/Release/net7.0/$p/publish -printf "%P\n" -type f -o -type l -o -type d | tar -czf out/$p.tar.gz --no-recursion -C bin/Release/net7.0/$p/publish -T -
 done
